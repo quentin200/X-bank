348eaca4219c5202a60968f53f40fe9573886f1f
MaximeAhatescsharp <163330777+MaximeAhatescsharp@users.noreply.github.com>
2024-11-14 11:56:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,70 @@
+import math
 
+#from terminalplot import plot
+#import asciichartpy as acp
+
+#import plotext as plt
+#import yfinance as yf
+
+import os
+import random
+
+#import bitcoin
+
+class Currency:
+    def __init__(self, name, value, total_shares, currency_id, is_real = False):
+        pass
+
+
+class Minigame(self):
+    def __init__(self, name, player):
+        pass
+        
+
+class Utilisateur:
+    def __init__(self, name, password, age, user_id, permissions = 1, balance = 0, wallet = {}, loans = {}, can_delete = False):
+        self.name = name
+        self.password = password
+        self.age = age
+        self.user_id = user_id
+        self.permissions = permissions
+        self.balance = balance
+        self.wallet = wallet
+        self.loans = loans
+        self.can_delete = can_delete
+
+        self.trust = 100
+
+    def delete(self):
+        pass
+        
+    def login(self):
+        pass
+        
+    def change_password(self):
+        pass
+    
+
+
+class Bank:
+    def __init__(self):
+        pass
+
+    def main(self):
+        pass
+        
+    def create_user(self, name, password, age):
+        pass
+        
+    def loan(self, user):
+        pass
+        
+    def check_persmissions(self, user):
+        pass
+
+    def minigame(self):
+        pass
+        
+                                              
+                                                              
+Bank()                                                            
